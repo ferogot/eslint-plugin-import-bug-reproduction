9d9c1836fb5fc10eb642130c7b24563e6dc3aaca
Alex Krohalev <a.krohalev@3divi.com>
2020-06-24 05:16:30 +0000

    did 'npx create-react-app min-app' created .eslintrc created 'eslinttest' which reproduce behaviour added command 'npm run eslint:test' for test behaviour

---
+++ min-app/eslinttest/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Comp2 from './use_default_index/Comp2';
+
+ReactDom.render(
+  <Comp2 />,
+  document.getElementById('root'),
+);
---
+++ min-app/eslinttest/use_default_index/Comp2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import {Component} from '../Component/index';
+
+function Comp2(props){
+
+  return (<Component />);
+}
+
+export default Comp2;
